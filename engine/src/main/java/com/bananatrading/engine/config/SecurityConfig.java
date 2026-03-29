aa3151b3a88375d19015687cb52e29838715dead
suman <sumanthegde32@gmail.com>
2026-03-29 18:04:04 +0530

    Fixed double slash in API URLs

--- engine/src/main/java/com/bananatrading/engine/config/SecurityConfig.java
+++ engine/src/main/java/com/bananatrading/engine/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.bananatrading.engine.config;
 import com.bananatrading.engine.security.JwtAuthFilter; // <-- Import your new filter
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -37,6 +38,7 @@ public class SecurityConfig {
         http
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/register", "/api/users/login","/ws-market/**").permitAll()
                         .anyRequest().authenticated() // Everything else is locked
